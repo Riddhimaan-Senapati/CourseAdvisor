6487998153e9a95bce37f9ada28947476409c9fb
Riddhimaan-Senapati <rsenapati@umass.edu>
2024-12-18 07:56:16 -0500

    added basic design to the prof page

--- pages/1_prof.py
+++ pages/1_prof.py
@@ -10,10 +10,39 @@ professor_name = st.text_input("Enter professor name")
 school=ratemyprofessor.get_school_by_name("University of Massachusetts Amherst")
 professor=ratemyprofessor.get_professor_by_school_and_name(school,professor_name)
 
+def create_scorecard(label, value, max_value=5.0):
+    color = "red"
+    if value > 3.5:
+        color = "green"
+    elif 2 <= value <= 3.5:
+        color = "yellow"
+    
+    st.markdown(f"""
+    <div style="text-align: center;">
+        <div style="
+            display: inline-block;
+            border: 2px solid {color};
+            border-radius: 50%;
+            width: 60px;
+            height: 60px;
+            line-height: 60px;
+            font-size: 24px;
+            color: {color};
+            font-weight: bold;
+        ">
+            {value:.1f}
+        </div>
+        <p>{label}: {value:.1f} / {max_value:.1f}</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+
 if professor is not None:
     st.write("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
-    st.write("Rating: %s / 5.0" % professor.rating)
-    st.write("Difficulty: %s / 5.0" % professor.difficulty)
+    create_scorecard("Rating", professor.rating)
+    create_scorecard("Difficulty", professor.difficulty)
+    #st.write("Rating: %s / 5.0" % professor.rating)
+    #st.write("Difficulty: %s / 5.0" % professor.difficulty)
     st.write("Total Ratings: %s" % professor.num_ratings)
     if professor.would_take_again is not None:
         st.write(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
