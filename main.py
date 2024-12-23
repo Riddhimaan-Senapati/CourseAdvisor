b314b216852bbd839267f2c696dbe759ad6814a8
Riddhimaan-Senapati <rsenapati@umass.edu>
2024-12-23 18:47:13 +0530

    updated the code to now ask for an openai api key instead of using my own api key. Updated the prof.py page as well to select the university as well.

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ from llama_index.packs.raptor import RaptorPack
 import nest_asyncio
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
-from llama_index.llms.groq import Groq
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
+import os
 
 load_dotenv()
 
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="feedback_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by Groq")
+
+st.title("Course Advisor")
+st.caption("🚀 A Streamlit chatbot powered by OpenAI and RAPTOR RAG")
 
 # File uploader for document input
 uploaded_file = st.file_uploader("Upload a PDF article", type="pdf")
@@ -33,11 +38,15 @@ for msg in st.session_state.messages:
 
 # Process user input and generate responses
 if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
     if not uploaded_file:
         st.info("Please upload a PDF file to continue.")
         st.stop()
 
-    # Initialize Groq client
+    # Initialize OpenAI client using aisuite library
+    os.environ["OPENAI_API_KEY"] = openai_api_key
     client = ai.Client()
 
     # Append user message to session state
@@ -51,24 +60,24 @@ if prompt := st.chat_input():
             with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
                 tmp_file.write(uploaded_file.getvalue())
                 temp_file_path = tmp_file.name
+
+            # Constructing the Clusters/Hierarchy Tree
             
             nest_asyncio.apply()
 
             documents = SimpleDirectoryReader(input_files=[temp_file_path]).load_data()
 
-            settings=Settings
-            settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
             from chromadb.config import Settings
             chroma_client = chromadb.PersistentClient(path="/tmp/.chroma", settings=Settings())
-            embedding_model_name = "BAAI/bge-small-en-v1.5"  # Choose your desired model here
-            hf_embeddings = HuggingFaceEmbedding(model_name=embedding_model_name)
+
             vector_store = ChromaVectorStore(
                 chroma_collection=chroma_client.get_or_create_collection(name='persistent_collection'),
-                embedding_function=hf_embeddings)
+                embedding_function=OpenAIEmbedding(model="text-embedding-3-small"))
+            
             raptor_pack = RaptorPack(
                 documents,
-                embed_model=settings.embed_model,
-                llm=Groq(model="llama3-70b-8192", temperature=0.1),
+                embed_model=OpenAIEmbedding(model="text-embedding-3-small"), 
+                llm=OpenAI(model="gpt-3.5-turbo", temperature=0.1),
                 vector_store=vector_store,  # used for storage
                 similarity_top_k=2,  # top k for each layer, or overall top-k for collapsed
                 mode="tree_traversal",  # sets default mode
@@ -77,9 +86,6 @@ if prompt := st.chat_input():
             st.session_state.raptor_pack=raptor_pack
 
 
-
-            
-
         st.success("PDF processed successfully!")
 
     # Prepare context for the LLM response
@@ -87,15 +93,13 @@ if prompt := st.chat_input():
     nodes = st.session_state.raptor_pack.run(prompt, mode="tree_traversal")
     context="\n".join([node.text for node in nodes])
 
-
-
     # Define a system message to guide the LLM's behavior
     system_message = {
         "role": "system",
         "content": """You are a course advisor meant to help students choose the courses that they are most suitable for.
-        You will be given some information about a course and you need to use that to explain a user's prompt.
-        If they ask about a course, give it's id as well for ex: Tell me about a course in AI: One such course is CS XXX
-        (replace with an actual number here) and instructors"""
+        You will be given some information about a course as context and you need to use that to explain a user's prompt.
+        If they ask you that is not course related. Respond with "Sorry,I can't help you". Do not respond to anything that
+        is not course related. Always in that case. Respond with "Sorry,I can't help you". """
     }
 
     # Combine system message, context, and conversation history for LLM input
@@ -104,16 +108,15 @@ if prompt := st.chat_input():
     # Generate a response from Groq's model using context and conversation history
     chat_completion = client.chat.completions.create(
         messages=messages_for_llm,
-        model="groq:llama3-70b-8192"  # Specify your desired model here
+        model="openai:gpt-3.5-turbo"  # Specify your desired model here
     )
 
     # Extract and display assistant's response
-    msg = chat_completion.choices[0].message.content  # Adjust based on actual response structure from Groq API
+    msg = chat_completion.choices[0].message.content  # Adjust based on actual response structure from Open AI API
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
     
-    # Create a clickable button
-    # Source documents
+    # Create a clickable button to show Source documents
     with st.expander("Source (summarized by LLM)"):
         st.write(context)
 
--- pages/1_prof.py
+++ pages/1_prof.py
@@ -1,35 +1,41 @@
 import ratemyprofessor
 import streamlit as st
 from components import create_donut_chart, create_percentage_donut
+import pandas as pd
 
-
-st.title("UMass Professor Rankings")
+# Title of the application
+st.title("Professor Rankings")
 st.caption("See how professors compare")
+
+# List of universities (replace this with your actual university list)
+df=pd.read_csv("universities.csv")
+universities=df["University Name"]
+
+# Dropdown for selecting a university
+selected_university = st.selectbox("Select a University", universities)
+
+# Input for professor name
 professor_name = st.text_input("Enter professor name")
-school=ratemyprofessor.get_school_by_name("University of Massachusetts Amherst")
-professor=ratemyprofessor.get_professor_by_school_and_name(school,professor_name)
 
- 
-if professor is not None:
+# Get school by selected university name
+school = ratemyprofessor.get_school_by_name(selected_university)
+if school is None:
+    st.write("Error! University not found")
+else:
+    professor = ratemyprofessor.get_professor_by_school_and_name(school, professor_name)
+    if professor is not None:
     # Create four columns for the components
-    col1, col2, col3, col4 = st.columns(4)
-    
-    with col1:
+       col1, col2, col3, col4 = st.columns(4)
+       with col1:
         create_donut_chart("Rating", professor.rating)
-    
-    with col2:
+       with col2:
         create_donut_chart("Difficulty", professor.difficulty)
-
-    with col3:
+       with col3:
         if professor.would_take_again is not None:
-            create_percentage_donut("Would take again %",professor.would_take_again)
+            create_percentage_donut("Would take again %", professor.would_take_again)
         else:
             st.write("Would Take Again: N/A")
-    
-    with col4:
+       with col4:
         st.metric(label="Total Ratings", value=f"{professor.num_ratings}")
-    
-else:
-    st.write("Error! Professor not found")
-
-
+    else:
+       st.write("Error! Professor not found")
